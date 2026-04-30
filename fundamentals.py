b91ab1ea8b5ccecb58e596e32791d686d4b7c200
josh-data-eng <ihimirejosh@gmail.com>
2026-04-30 13:34:57 +0100

    changed file name from data-types to fundamentals

---
+++ fundamentals.py
@@ -0,0 +1,26 @@
+# DATA TYPES
+name = "Josh"
+age = 23
+student = True
+height = 25.4
+
+# PRINT FUNCTION
+print(type(name))
+print(type(age))
+print(type(student))
+print(type(height))
+
+#ESCAPE SEQUENCE
+print('''Your Learning
+      -Python Basics
+      -Data Engineering
+      -AI''')
+
+# INPUT FUNCTION
+first_name = input("Enter Firstname:")
+last_name = input("Enter Lastname:")
+age = input("Enter Age:")
+full_name = first_name +" " + last_name
+print(full_name)
+
+
