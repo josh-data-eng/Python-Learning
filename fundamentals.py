b5ee31f6b090e508f543f1621badfd842df54562
josh-data-eng <ihimirejosh@gmail.com>
2026-05-01 14:06:05 +0100

    learnt and added basic strings manipulation methods, split, slicing, replace... etc

--- fundamentals.py
+++ fundamentals.py
@@ -17,10 +17,22 @@ print('''Your Learning
       -AI''')
 
 # INPUT FUNCTION
-first_name = input("Enter Firstname:")
-last_name = input("Enter Lastname:")
-age = input("Enter Age:")
-full_name = first_name +" " + last_name
-print(full_name)
+#first_name = input("Enter Firstname:")
+#last_name = input("Enter Lastname:")
+#age = input("Enter Age:")
+#full_name = first_name +" " + last_name
+#print(full_name)
 
+#SLICING AND INDEXING
+full_name = "Joshua Ihimire"
+print(full_name[7])
+print(full_name[0:6])
+print(full_name[-7:])
+print(full_name[::2])
 
+print("-" *20)
+
+date = "1-5-2026"
+print(date.split("-"))
+print(date.replace("0","5"))
+print(f"my name is {full_name} iam {age} iam {height}cm tall my birthday is {date}")
